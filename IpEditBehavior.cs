35174a8eecce730204664d5ac87325be717d7852
Aleksandr Riapolov <aRyapolov@ntoire-polus.ru>
2021-04-15 16:10:18 +0300

    Initial code commit

---
+++ IpEditBehavior.cs
@@ -0,0 +1,64 @@
+﻿using System.Data;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace AR.WPF.Behaviors
+{
+    public class IpEditBehavior : Behavior<TextBox>
+    {
+        private string _ip = "";
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            this.AssociatedObject.TextChanged += AssociatedObject_TextChanged;
+            _ip = this.AssociatedObject.Text;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            this.AssociatedObject.TextChanged -= AssociatedObject_TextChanged;
+        }
+
+        
+
+        private void AssociatedObject_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var pointsCount = this.AssociatedObject.Text.Count(c => c == '.');
+            var splt = this.AssociatedObject.Text.Split('.');
+
+            if (pointsCount > 3)
+                UndoChange(e);
+            else
+            {
+                foreach (var ot in splt)
+                    if (!VerifyOctetText(ot, e))
+                        return;
+                _ip = this.AssociatedObject.Text;
+            }
+        }
+
+        private bool VerifyOctetText(string txt, TextChangedEventArgs e)
+        {
+            if ((byte.TryParse(txt, out var oct) && txt.Length <= 3) || string.IsNullOrEmpty(txt))
+            {
+                return true;
+            }
+
+            UndoChange(e);
+            return false;
+        }
+
+        private void UndoChange(TextChangedEventArgs e)
+        {
+            this.AssociatedObject.Text = _ip;
+            var firstChange = e.Changes.First();
+            if (firstChange != null)
+                this.AssociatedObject.CaretIndex = firstChange.Offset;
+        }
+
+    }
+}
